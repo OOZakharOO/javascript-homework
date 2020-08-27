6a1775e44436a8862dff727ecf3a2398d299ac5f
OOZakharOO <stolyarzahar1@gmail.com>
2020-08-27 17:36:51 +0300

    hw 10

---
+++ src/hw.js
@@ -0,0 +1,35 @@
+import menu from "./menu.json";
+console.log(menu);
+import menuItemTemplate from "./menu-item.hbs";
+
+const item = menuItemTemplate(menu);
+const menuList = document.querySelector(".js-menu");
+menuList.insertAdjacentHTML("afterbegin", item);
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+
+const switchBtn = document.querySelector(".js-switch-input");
+const section = document.querySelector(".module-10");
+console.log(switchBtn);
+
+if (localStorage.getItem("colorTheme")) {
+  section.classList.add(Theme.DARK);
+  switchBtn.checked = true;
+}
+
+switchBtn.addEventListener("change", () => {
+  console.dir(switchBtn);
+
+  if (switchBtn.checked) {
+    localStorage.setItem("colorTheme", " ");
+    section.classList.add(Theme.DARK);
+    section.classList.remove(Theme.LIGHT);
+  } else {
+    localStorage.removeItem("colorTheme");
+    section.classList.remove(Theme.DARK);
+    section.classList.add(Theme.LIGHT);
+  }
+});
